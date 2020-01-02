4a4221e066ad3ff2635c38820237629464070933
Daniel <daniel_sousa.p@outlook.com>
2020-01-02 14:23:53 -0300

    Tela de Login

--- mobile/src/components/SpotList.js
+++ mobile/src/components/SpotList.js
@@ -37,7 +37,7 @@ function SpotList({ tech, navigation }) {
             renderItem={({ item }) => (
                 <View style={styles.listItem}> 
                     {/* utilizei esse replace somente para exibir a imagem localmente */}
-                    <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url.replace('localhost', '192.168.25.13')}} />
+                    <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url.replace('localhost', '192.168.25.8')}} />
                     <Text style={styles.company}>{item.company}</Text>
                     <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
                     <TouchableOpacity onPress={() => handleNavigate(item._id)}style={styles.button}>
